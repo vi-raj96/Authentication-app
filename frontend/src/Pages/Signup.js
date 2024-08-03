f3bddad6aeb33aa3d46c41c9aa93ae3836c6ca7c
Vijay Rajak <60967897+vi-raj96@users.noreply.github.com>
2024-08-03 14:23:57 +0530

    Update Signup.js

--- frontend/src/Pages/Signup.js
+++ frontend/src/Pages/Signup.js
@@ -35,7 +35,7 @@ function Signup() {
             return handleError('Password is required')
         }
         try {
-            const url = "http://localhost:9090/auth/signup";
+            const url = "https://authentication-app-api-sigma.vercel.app/auth/signup";
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
